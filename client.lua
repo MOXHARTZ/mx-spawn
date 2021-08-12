da78c9e3500f1c1228c150b56094a8ab150609c4
MOXHAFOREVA <70913098+MOXHAFOREVA@users.noreply.github.com>
2021-08-12 22:11:29 +0300

    2.0 RELEASE

--- client.lua
+++ client.lua
@@ -17,20 +17,15 @@ AddEventHandler('mx-spawn:Open', function (data)
      SetNuiFocus(true, true)
      SendNUIMessage({
           type = 'open',
+          locs = Cfg
      })
 end)
 
 RegisterNUICallback('Spawn', function (data)
-     SetEntityCoords(PlayerPedId(), vector3(424.99, -978.32, 30.71))
+     FreezeEntityPosition(PlayerPedId(), true)
      SetNuiFocus(false, false)
      local timer = GetGameTimer()
-     if data.spawnLoc == 'lastlocation' then
-          TriggerServerEvent('mx-multicharacter:GetLastLoc')       
-          while not LastLoc do
-               Wait(100)
-          end   
-          SetEntityCoords(PlayerPedId(), LastLoc)
-     else
+     if data.spawnLoc ~= 'lastlocation' then
           SetEntityCoords(PlayerPedId(), Cfg.Locs[data.spawnLoc])
      end
      if not IsScreenFadedOut() then
@@ -60,25 +55,7 @@ RegisterNUICallback('Spawn', function (data)
      while GetPlayerSwitchState() ~= 12 do
          Wait(0)
      end
-
      Wait(1000)
-
-     if CurrentData then
-          TriggerServerEvent('mx-multicharacter:CreateCharacter', CurrentData)
-          while not exports['mx-multicharacter']:GetCid() do
-               print('Is Nil')
-               Wait(100)
-          end
-     end
-     
-     if CurrentData then
-          if exports['mx-multicharacter']:GetUseEssential() then
-               TriggerServerEvent('es:firstJoinProper')
-               TriggerEvent('es:allowedToSpawn')
-               TriggerEvent('esx:CharCreate', CurrentData)
-          end
-          TriggerServerEvent('esx:onPlayerJoined', CurrentData)   
-     end
      FreezeEntityPosition(PlayerPedId(), false)
      SetEntityVisible(PlayerPedId(), true)
 end)
